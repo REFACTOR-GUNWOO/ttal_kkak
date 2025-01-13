bcfea86eeb12cd0d920b2c1b36b45e07a7fc8ac2
REFACTOR_GUNWOO <gunwoo@frientrip.com>
2025-01-14 00:42:30 +0900

    FEAT : 스크롤 로직 수정

--- lib/provider/scroll_controller_provider.dart
+++ lib/provider/scroll_controller_provider.dart
@@ -2,13 +2,26 @@ import 'package:flutter/material.dart';
 
 class ScrollControllerProvider extends ChangeNotifier {
   final ScrollController scrollController = ScrollController();
+  bool isScrolling = false;
 
   void scrollToTop() {
-    scrollController.animateTo(
-      0.0, // 맨 위로 이동
-      duration: Duration(milliseconds: 500),
-      curve: Curves.easeInOut,
-    );
+    if (scrollController.hasClients && !isScrolling) {
+      isScrolling = true;
+      scrollController
+          .animateTo(
+        0.0,
+        duration: Duration(milliseconds: 500),
+        curve: Curves.easeInOut,
+      )
+          .whenComplete(() {
+        isScrolling = false;
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
   }
 }
-
