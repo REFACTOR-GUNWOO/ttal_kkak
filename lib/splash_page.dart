31b28285b0b85a1452e78216c3792e2571a581fe
REFACTOR_GUNWOO <gunwoo@frientrip.com>
2025-02-19 17:57:44 +0900

    FEAT : 스플래시 페이지 디자인 수정

--- lib/splash_page.dart
+++ lib/splash_page.dart
@@ -1,26 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:lottie/lottie.dart';
 import 'package:ttal_kkak/clothes_repository.dart';
 import 'package:ttal_kkak/is_newbie_repository.dart';
 import 'package:ttal_kkak/main_layout.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ttal_kkak/onboarding_page.dart';
+import 'package:ttal_kkak/styles/colors_styles.dart';
+import 'package:ttal_kkak/styles/text_styles.dart';
 
 class SplashPage extends StatefulWidget {
   @override
   _SplashPageState createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> {
+class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
+  late final AnimationController _controller;
+
   @override
   void initState() {
     super.initState();
     // ClothesRepository().deleteAllClothes();
     _navigateToHome();
+    _controller = AnimationController(vsync: this);
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.edgeToEdge);
+    super.dispose();
   }
 
   _navigateToHome() async {
     // 스플래시 화면을 3초 동안 표시한 후 홈 화면으로 이동합니다.
-    await Future.delayed(Duration(seconds: 3), () {});
+    await Future.delayed(Duration(seconds: 4), () {});
     _checkButtonClickStatus();
   }
 
@@ -52,13 +68,29 @@ class _SplashPageState extends State<SplashPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
-              "내 첫 모바일 옷장",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            SvgPicture.asset("assets/icons/splash_icon.svg"),
+            SizedBox(
+              height: 12,
             ),
             Text(
-              "딸깍",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              "내 첫 모바일 옷장 딸깍",
+              style: BodyTextStyles.Regular16.copyWith(
+                  color: SystemColors.gray900),
+            ),
+            SizedBox(
+              height: 36,
+            ),
+            Padding(
+              child: Lottie.asset(
+                "assets/lotties/splash_lottie.json",
+                controller: _controller,
+                onLoaded: (composition) {
+                  _controller
+                    ..duration = composition.duration
+                    ..forward();
+                },
+              ),
+              padding: EdgeInsets.only(left: 20, right: 20),
             )
           ],
         ),
