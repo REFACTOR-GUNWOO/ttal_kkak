1d3f1ec09b81130962a9495ec18ea3dd1bba3981
REFACTOR_GUNWOO <gunwoo@frientrip.com>
2025-03-01 18:43:02 +0900

    FEAT : 드로잉 등록 시에도 토스트 메시지 뜨도록 수정

--- lib/addClothesBottomSheet/detail_drawing_page.dart
+++ lib/addClothesBottomSheet/detail_drawing_page.dart
@@ -350,6 +350,7 @@ class _DetailDrawingPageState extends State<DetailDrawingPage> {
                           .clear(),
                       Provider.of<ReloadHomeProvider>(context, listen: false)
                           .triggerReload(),
+                      showToast(widget.isUpdate ? "수정되었습니다" : "등록되었습니다"),
                       Navigator.of(context).pushReplacement(
                         MaterialPageRoute(builder: (context) => MainLayout()),
                       )
